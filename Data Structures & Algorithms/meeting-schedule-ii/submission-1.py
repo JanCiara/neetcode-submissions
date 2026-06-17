61cbdcaf9d2c0a68b6455902aadcac3ec1a6b524
Jan Ciara <janek1.ciara@gmail.com>
2026-06-17 19:19:16 +0200

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.py
@@ -0,0 +1,18 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if not intervals:
+            return 0
+        mp = defaultdict(int)
+        for interval in intervals:
+            for i in range(interval.start, interval.end):
+                mp[i] += 1         
+        
+        return max(mp.values())
