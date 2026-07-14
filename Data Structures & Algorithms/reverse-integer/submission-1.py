ea2dd0e0450bee1e73f1079394dfd0112f514e03
Jan Ciara <janek1.ciara@gmail.com>
2026-07-14 21:35:38 +0200

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        INT_MAX = 2**31 - 1
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        res = 0
+        while x:
+            digit = x % 10
+            x //= 10
+            # sprawdzamy PRZED pomnożeniem, żeby nigdy nie przekroczyć limitu
+            if res > (INT_MAX - digit) // 10:
+                return 0
+            res = res * 10 + digit
+        return sign * res
